03348754075d43bba799d1c8f7358748a6e2f529
Daniel Ferreira <danielfferreirabr@gmail.com>
2022-01-27 23:40:25 -0300

    criando componente app e chat

--- pages/index.js
+++ pages/index.js
@@ -3,37 +3,6 @@ import React from "react";
 import { useRouter } from 'next/router';
 import appConfig from "../config.json";
 
-function GlobalStyle() {
-	return (
-		<style global jsx>{`
-			* {
-				margin: 0;
-				padding: 0;
-				box-sizing: border-box;
-				list-style: none;
-			}
-			body {
-				font-family: "Open Sans", sans-serif;
-			}
-			/* App fit Height */
-			html,
-			body,
-			#__next {
-				min-height: 100vh;
-				display: flex;
-				flex: 1;
-			}
-			#__next {
-				flex: 1;
-			}
-			#__next > * {
-				flex: 1;
-			}
-			/* ./App fit Height */
-		`}</style>
-	);
-}
-
 function Titulo(props) {
 	console.log(props);
 	const Tag = props.tag || 'h1';
@@ -67,11 +36,8 @@ export default function PaginaInicial() {
 	const [username, setusername] = React.useState('');
 	const roteamento = useRouter();
 
-	console.log(roteamento);
-
 	return (
-		<>
-			<GlobalStyle />
+		<>	
 			<Box
 				styleSheet={{
 					display: "flex",
